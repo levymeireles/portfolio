e69939e380fc63ee1cfd6a182dbd13aea7885cea
levymeireles <levymeireles518@gmail.com>
2020-12-15 13:14:36 -0300

    commit with VScode

---
+++ src/components/sections/Interests.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Interests = () => {
+
+
+    return (
+
+        <section
+            className="resume-section p-3 p-lg-5 d-flex align-items-center"
+            id="interests"
+        >
+            <div className="w-100">
+                <h2 className="mb-5">Interesses</h2>
+                <p>
+                    Além de desenvolvedor sou grande amante dos avanços tecnológicos e desde sempre gostei de computadores e afins
+                    hoje em dia sou entusiasta da tecnologia e acompanho a área de exploração espacial e de smartphones.
+                </p>
+                <p className="mb-0">
+                    No meu tempo livre gosto de estudar, ler e assisto algumas séries e filmes de ficção também. Em momentos de lazer gosto de 
+                    curtir com meus amigos ou até mesmo fazer eventos como hobby ou freelance.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default Interests

a6270801063e04a1babb66ba01d06fa657a18f52
levymeireles <levymeireles518@gmail.com>
2021-03-03 19:39:44 -0300

    alteracao no portfolio

--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -16,12 +16,26 @@ const Experience = () => {
                 <h3 className="mb-0">Estagiário de Desenvolvimento</h3>
                 <div className="subheading mb-3">Evolucional</div>
                 <p>
-                    Atualmente trabalho como estagiário na startup Evolucional na área de desenvolvimento, 
+                    Trabalhei como estagiário na startup Evolucional na área de desenvolvimento, 
                     cuidando e criando as páginas e funções para o sistema interno da empresa.
                 </p>
                 </div>
                 <div className="resume-date text-md-right">
-                <span className="text-primary">Março 2020 - Atualmente</span>
+                <span className="text-primary">Março 2020 - Janeiro 2021</span>
+                </div>
+            </div>
+
+            <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div className="resume-content">
+                <h3 className="mb-0">Desenvolvedor Júnior</h3>
+                <div className="subheading mb-3">Evolucional</div>
+                <p>
+                    Atualmente trabalho como desenvolvedor júnior na startup Evolucional na área de desenvolvimento, 
+                    criando e implementando features, telas, API entre outras demandas da empresa.
+                </p>
+                </div>
+                <div className="resume-date text-md-right">
+                <span className="text-primary">Feveiro 2021 - Atualmente</span>
                 </div>
             </div>
         </div>
